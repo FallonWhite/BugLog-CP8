c48aa52bc502b083d596af2d078ff4ca5de0ae8f
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-08-13 22:55:39 -0600

    Break

--- BugLog-CP8/server/services/BugsService.js
+++ BugLog-CP8/server/services/BugsService.js
@@ -1,9 +1,33 @@
 import { dbContext } from '../db/DbContext'
+import { BadRequest } from '../utils/Errors'
 
 class BugsService {
   async create(body) {
     const bug = await dbContext.Bugs.create(body)
     return await dbContext.Bugs.findById(bug._id).populate('creator', 'name picture')
+  }
+
+  async getAll(query = {}) {
+    return await dbContext.Bugs.find(query).populate('creator', 'name picture')
+  }
+
+  async getById(bugId) {
+    const bug = await dbContext.Bugs.findById(bugId).populate('creator', 'name picture')
+    if (!bug) {
+      throw new BadRequest('Invalid Id')
+    }
+    return bug
+  }
+
+  async edit(body, user) {
+    const bug = await this.getById(body.id)
+    if (user.id === bug.creatorId.toString()) {
+      const bug = await dbContext.Bugs.findByIdAndUpdate(body.id, body, { new: true, runValidators: false })
+      if (!bug) {
+        throw new BadRequest('Edit Unsuccessful')
+      }
+      return bug
+    }
   }
 }
 
