29492ff1b33b680dbaefb17ddf0440f2f78f9908
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-08-14 13:13:33 -0600

    DBCOnetext.NOtes

--- BugLog-CP8/server/db/DbContext.js
+++ BugLog-CP8/server/db/DbContext.js
@@ -2,11 +2,14 @@ import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { Bug } from '../models/Bug'
 import { Value as ValueSchema } from '../models/Value'
+import { Note } from '../models/Note'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema)
   Bugs = mongoose.model('Bug', Bug)
   Account = mongoose.model('Account', AccountSchema)
+
+  Notes = mongoose.model('Note', Note)
 }
 
 export const dbContext = new DbContext()
