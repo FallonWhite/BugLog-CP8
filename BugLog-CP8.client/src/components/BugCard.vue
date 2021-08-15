826ad7ac9f8725cbb5d14cddf58cb24130c68a28
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-08-15 14:19:22 -0600

    break

---
+++ BugLog-CP8.client/src/components/BugCard.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="card text-white bg-info mb-3 m-4 justify-content-center" style="">
+    <div class="card-header text-center">
+      <h5 class="p-2" style="text-align: center; text-shadow: 2px 2px black; background-color: grey">
+        <router-link router-link :to="{ name: 'Bug Details', params: {id: bugProp.id } }">
+          Bug
+        </router-link>
+        <!-- </router-link> -->
+      </h5>
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">
+        {{ bugProp.title }}
+      </h5>
+      <p class="card-text">
+        {{ bugProp.description }}
+      </p>
+      <p class="card-text">
+        {{ bugProp.closed }}
+      </p>
+      <p class="card-text">
+        {{ bugProp.closedDate }}
+      </p>
+      <div class="align-self-end" v-if="account.id === bugProp.creatorId">
+        <button class="btn-sm btn-dark" style="background-color: brown" @click="destroyProject">
+          Close Bug
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed } from '@vue/runtime-core'
+import { AppState } from '../AppState'
+import Pop from '../utils/Notifier'
+import { bugsService } from '../services/BugsService'
+export default {
+  props: {
+    bugProp: {
+      type: Object,
+      required: true
+    }
+  },
+  setup(props) {
+    return {
+      account: computed(() => AppState.account),
+      async destroy() {
+        try {
+          if (await Pop.confirm()) {
+            await bugsService.destroy(props.bugProp.id)
+            Pop.toast('Bug Closed', 'Success!')
+          }
+        } catch (error) {
+          Pop.toast(error, 'error')
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.card{
+  width: 500vh;
+}
+</style>
--- BugLog-CP8.client/src/pages/HomePage.vue
+++ BugLog-CP8.client/src/pages/HomePage.vue
@@ -1,14 +1,54 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <!-- <h1 class="my-5 bg-dark text-light p-3 rounded d-flex align-items-center">
-      <span class="mx-2 text-white">Bug Log</span>
-    </h1> -->
+  <!-- <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center"> -->
+  <div class="container-fluid">
+    <div class="row mt-4">
+      <div class="col-12 text-right">
+        <button class="btn btn-outline-secondary btn-info text-dark mr-5" data-target="#bug-modal" data-toggle="modal">
+          <b>Create Bug</b>
+        </button>
+      </div>
+    </div>
+    <!-- <div class="row">
+      <div class="col-12">
+        <h1 class="text-info p-4 m-4" style="text-align: center; text-shadow: 2px 2px black; background-color: grey">
+          <button class="btn btn-outline-secondary btn-info text-center mr-5 text-dark" data-target="#bug-modal" data-toggle="modal">
+            <b>Create Bug</b>
+          </button>
+        </h1>
+      </div> -->
+    <div class="row">
+      <div class="col-4" v-for="b in bugs" :key="b.id">
+        <BugCard :bug="b" />
+      </div>
+    </div>
   </div>
+  <router-view>
+  </router-view>
 </template>
 
 <script>
+import { computed, onMounted } from '@vue/runtime-core'
+import { AppState } from '../AppState.js'
+import { bugsService } from '../services/BugsService'
+import BugCard from '../components/BugCard.vue'
+import { logger } from '../utils/Logger'
 export default {
-  name: 'Home'
+  name: 'Home',
+  setup() {
+    onMounted(async() => {
+      try {
+        await bugsService.getBugs()
+      } catch (error) {
+        logger.log(error)
+      }
+    })
+    return {
+      bugs: computed(() => AppState.bugs)
+    }
+  },
+  components: {
+    BugCard
+  }
 }
 </script>
 
