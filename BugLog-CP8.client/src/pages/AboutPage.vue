63be6c8883c9fb27d9cca94d296e9e896a0640be
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-08-15 14:48:02 -0600

    AddedAboutDuringCodingBreak

--- BugLog-CP8.client/src/pages/AboutPage.vue
+++ BugLog-CP8.client/src/pages/AboutPage.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="about">
-    <h1 class="m-5">
-      This is the about page
+    <h1 class="m-5 mt-1">
+      Welcome to the About section of Bug Log Station, established in 2021 during the Great Bootcamp Experiment 🐛🐜🐛🐜🐛.
     </h1>
+    <p class="m-5">
+      Bugs may be cute outside, but they have no business marching around freely within our, or your system(s). So let's start logging those little problem-causing guys and gals into the tracker, and try to identify where they are coming from, how long it takes to remove them; maybe we can even identify potential routes of access and keep them from freely roaming and making a mess of things. <br />
+
+      To start using the tracker, make sure to register. The navigation of the system is quite intuitive, but as quick run-down on the "how-to's" are below:<br />
+      From the Home Page, you can navigate to your account info from the naviagtion bar, on the right.
+      On the left on the navigation bar, you'll find quick links to get you where you need to go, which you must already know since you found this page!<br />
+      By clicking the Create Bug button, found on the right, you'll open a window to be prompted to enter in the information regarding your little not-so-friendly software villian.<br />
+      Once you have created a New Bug, you can click on the Title of that little guy or gal, and that will take you to the Bug Details Page. It's on that page that you can start adding notes, and update/close the Bug.
+    </p>
   </div>
 </template>
 
