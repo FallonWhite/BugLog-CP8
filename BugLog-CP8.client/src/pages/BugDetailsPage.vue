45a03f3a26daab4c50cde64d3c6fefc84e81abf2
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-08-14 22:27:48 -0600

    AddedBugDetailsPage

---
+++ BugLog-CP8.client/src/pages/BugDetailsPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bugDetails">
+    <h1 class="m-5">
+      This is the Bug Details page
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BugDetails',
+  setup() {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
